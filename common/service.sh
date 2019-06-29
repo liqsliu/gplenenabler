2857b87cf34b24c41e3038135ceb194d96bff580
lindroidux <51092143+lindroidux@users.noreply.github.com>
2019-06-29 21:02:35 +0530

    updated sleep to 60 seconds in service.sh

--- common/service.sh
+++ common/service.sh
@@ -7,10 +7,10 @@
 MODDIR=${0%/*}
 
 
-#Check, every 15 seconds. whether an instance of dnscrypt-proxy is running,if not, keep looping
+#Check, every 60 seconds. whether an instance of dnscrypt-proxy is running,if not, keep looping
 # till network is available & start dnscrypt-proxy.
 
 	while ! [ `pgrep -x dnscrypt-proxy` ] ; do
-		$MODDIR/system/bin/dnscrypt-proxy -config  /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml && sleep 15;
+		$MODDIR/system/bin/dnscrypt-proxy -config  /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml && sleep 60;
 	done
 
