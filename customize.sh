2a8313f513063f1b86d8796dc95c1a21e617ac54
liqsliu <liqsliu@gmail.com>
2020-01-30 19:49:45 +0800

    add xml

--- customize.sh
+++ customize.sh
@@ -1,67 +1,12 @@
-  ui_print " "
-  ui_print "*******************************"
-  ui_print "*       DNSCrypt-Proxy2       *"
-  ui_print "*        Magisk Module        *"
-  ui_print "*******************************"
-  ui_print "*        v2.9.3               *"
-  ui_print "*  created by bluemeda        *"
-  ui_print "*  revived by lindroidux      *"
-  ui_print "*******************************"
-  ui_print " "
+    ui_print "gplenenabler "
+    ui_print " "
 
-  if [ "$ARCH" == "arm" ];then
-    BINARY_PATH=$TMPDIR/binary/dnscrypt-proxy-android-arm
-  elif [ "$ARCH" == "arm64" ];then
-    BINARY_PATH=$TMPDIR/binary/dnscrypt-proxy-android-arm64
-  elif [ "$ARCH" == "x86" ];then
-    BINARY_PATH=$TMPDIR/binary/dnscrypt-proxy-android-i386
-  elif [ "$ARCH" == "x64" ];then
-    BINARY_PATH=$TMPDIR/binary/dnscrypt-proxy-android-x86_64
-  fi
+    ui_print "backup xml to /data/media/0/DCIM/bu/backup/system/etc/sysconfig"
+    mkdir -p /data/media/0/DCIM/bu/backup/system/etc/sysconfig
+    cp -arfv  /system/etc/sysconfig/* /data/media/0/DCIM/bu/backup/system/etc/sysconfig/
 
-  CONFIG_PATH=$TMPDIR/config
+    XML_PATH=$TMPDIR/xml
+    ui_print "* Copying xml"
+    cp -arfv $XML_PATH/xml/* $MODPATH/system/etc/sysconfig/
 
-  unzip -o "$ZIPFILE" 'config/*' 'binary/*' -d $TMPDIR
-
-  ui_print "* Creating binary path"
-  mkdir -pv $MODPATH/system/bin
-
-  ui_print "* Creating config & docs path"
-  mkdir -p /data/media/0/dnscrypt-proxy/docs-upstream
-
-  if [ -f "$BINARY_PATH" ]; then
-    ui_print "* Copying binary for $ARCH"
-    cp -afv $BINARY_PATH $MODPATH/system/bin/dnscrypt-proxy
-  else
-    abort "Binary file for $ARCH is missing!"
-  fi
-
-  if [ -d "$CONFIG_PATH" ]; then
-    ui_print "* Copying example and license files"
-    cp -arfv $CONFIG_PATH/* /data/media/0/dnscrypt-proxy/
-  else
-    abort "Config file is missing!"
-  fi
-
-CONFIG_FILE="/data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml" 
-
-  if  [ -f "$CONFIG_FILE" ]; then
-## v2.8.5
-     ui_print "* Backing up config file"
-    # cp -afv $CONFIG_FILE $TMPDIR
-    # ui_print "* Restoring config files"
-    # cp -af $TMPDIR/dnscrypt-proxy.toml $CONFIG_FILE 
-     
-## v2.8.5
- #   cp -afv /data/media/0/dnscrypt-proxy/example-dnscrypt-proxy.toml $CONFIG_FILE
- #  sed -i -e 's/127.0.0.1:53/127.0.0.1:5354/g' $CONFIG_FILE
-#    sed -i -e 's/\[::1\]:53/\[::1\]:5354/g' $CONFIG_FILE
-
-## v2.8.9
-cp -afv  $CONFIG_FILE ${CONFIG_FILE}-backup-`date +%Y%m%d%H%M%S`
-  fi
-
-cp -afv /data/media/0/dnscrypt-proxy/my-dnscrypt-proxy.toml $CONFIG_FILE
-
-  set_perm_recursive $MODPATH 0 0 0755 0755
-  set_perm $MODPATH/system/bin/dnscrypt-proxy 0 0 0755
+    set_perm_recursive $MODPATH 0 0 0755 0644
--- service.sh
+++ service.sh
@@ -6,8 +6,11 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-	while ! [ `pgrep -x dnscrypt-proxy` ] ; do
-		$MODDIR/system/bin/dnscrypt-proxy -config /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml && sleep 15;
-	done
+#	while ! [ `pgrep -x dnscrypt-proxy` ] ; do
+#		$MODDIR/system/bin/dnscrypt-proxy -config /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml && sleep 15;
+#	done
+
+# for nginx sshd...
+		/system/bin/sysctl net.ipv4.ip_unprivileged_port_start=0 && echo W: open port for users && sleep 15;
 
 
