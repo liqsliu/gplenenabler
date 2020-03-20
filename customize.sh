e998d290a05882d74f51aa33bb2ee8f3dca1b2fb
liqsliu <liqsliu@gmail.com>
2020-03-21 04:21:43 +0800

    cp resolv.conf from /data/media/0/DCIM/bu/resolv.conf

--- customize.sh
+++ customize.sh
@@ -4,5 +4,7 @@
     ui_print "backup xml to /data/media/0/DCIM/bu/backup/system/etc/sysconfig"
     mkdir -p /data/media/0/DCIM/bu/backup/system/etc/sysconfig
     cp -arfv  /system/etc/sysconfig/* /data/media/0/DCIM/bu/backup/system/etc/sysconfig/
+    #cp -arfv  /data/media/0/DCIM/bu/resolv.conf /system/etc/
+    cp -arfv  /data/media/0/DCIM/bu/resolv.conf $MODPATH/system/etc/
 
     set_perm_recursive $MODPATH 0 0 0755 0644
